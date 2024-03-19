fc4ba040cc611dd44ba6f582d579e0138976933b
gabhiaf98 <gabhiaf98@gmail.com>
2024-03-19 17:51:17 +0000

    Mindmap Implemented

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import ReactFlow from "reactflow";
+import "reactflow/dist/style.css";
+import { initialNodes, initialEdges } from "./mockData/nodeData";
+import nodeView from "./components/nodeView";
+
+function App() {
+  return (
+    <div className="h-screen w-screen">
+      <ReactFlow
+        nodes={initialNodes}
+        edges={initialEdges}
+        nodeTypes={{ custom: nodeView }}
+        fitView
+        attributionPosition="bottom-left"
+      />
+    </div>
+  );
+}
+
+export default App;
